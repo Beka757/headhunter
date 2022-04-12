d24973d5265f2359eed781a3cc8c239d8fa03201
Danil <danilastolyarov97@gmail.com>
2022-04-12 17:01:16 +0600

    Создал кастомную модель пользователя

--- headhunter/accounts/models.py
+++ headhunter/accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    email = models.EmailField(max_length=255, blank=False, verbose_name="Email")
+    phone = PhoneNumberField(unique=True, blank=False, verbose_name="Номер телефона")
+    is_company = models.BooleanField(default=False, verbose_name="Роль")
+    image = models.ImageField(null=True, blank=True, upload_to='user_pics', verbose_name='Фото профиля')
+
